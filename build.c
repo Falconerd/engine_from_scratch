1cfe74e8aa5d41dd277285da7c1cc9d6c6a714cb
Dylan Falconer <me@falconerd.com>
2023-12-22 12:16:26 +1100

    Center window. Global state struct. Update build to fix stack issues

--- build.c
+++ build.c
@@ -1,9 +1,23 @@
 #include <windows.h>
 #include <stdio.h>
 #include <string.h>
-#define CFLAGS "-nologo -GR- -Oi"
-#define LIBPATH ".\\lib"
+/*
+    -nologo: Disable the M$/Windows logo crap.
+    -GR-: Disable run-time type information.
+    -GS-: Disable stack checking.
+    -Gs1000000000: Turn off stack probes.
+    -Oi: Enable compiler intrinsics.
+*/
+#define CFLAGS "-nologo -GR- -GS- -Gs1000000000 -Oi"
 #define CC "cl"
+
+/*
+    -nodefaultlib: Don't link CRT or auto-link kernel32 etc.
+    -subsytem:windows: Create a windows application (not console).
+    -stack...: Commit a larger stack.
+*/
+#define LINK_ARGS "-nodefaultlib -subsystem:windows -stack:0x200000,200000"
+#define LIBS "kernel32.lib user32.lib"
 
 int main(int argc, char *argv[]) {
     LARGE_INTEGER frequency, start, end;
@@ -28,7 +42,7 @@ int main(int argc, char *argv[]) {
 
     char buf[512] = {0};
     const char *optimization = release ? "/O2" : "/Zi";
-    sprintf(buf, "%s %s %s src\\main.c -Fe:_main.exe -link -nodefaultlib -subsystem:windows -libpath:%s kernel32.lib user32.lib gdi32.lib", CC, CFLAGS, optimization, LIBPATH);
+    sprintf(buf, "%s %s %s src\\main.c -Fe:_main.exe -link %s %s", CC, CFLAGS, optimization, LINK_ARGS, LIBS);
     printf("%s\n", buf);
     system(buf);
 
--- src/main.c
+++ src/main.c
@@ -6,36 +6,42 @@
 #define TITLE "Test"
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
-b32 running = 1;
+
+static struct state {
+    b32 running;
+} state = {
+    .running = 1,
+};
 
 int __stdcall WinMain(void *instance, void *prev_instance, const char *command_line, int show_code) {
     WNDCLASSA wc = {0, DefWindowProc, 0, 0, 0, 0, 0, 0, 0, TITLE};
 
     if (!RegisterClass(&wc)) {
-        ErrorBox("Failed to register window class");
+        MessageBox(0, "Failed to register window class", "Error", 0);
         return 1;
     }
 
     int screen_width = GetSystemMetrics(SM_CXSCREEN);
     int screen_height = GetSystemMetrics(SM_CYSCREEN);
-    int x = (screen_width - WINDOW_WIDTH) / 2;
-    int y = (screen_height - WINDOW_HEIGHT) / 2;
+    int window_position_x = (screen_width - WINDOW_WIDTH) / 2;
+    int window_position_y = (screen_height - WINDOW_HEIGHT) / 2;
 
     HWND window = CreateWindow(0, TITLE, TITLE, WS_POPUP | WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-                               x, y, WINDOW_WIDTH, WINDOW_HEIGHT, 0, 0, 0, 0);
+                               window_position_x, window_position_y, WINDOW_WIDTH, WINDOW_HEIGHT,
+                               0, 0, 0, 0);
     if (!window) {
-        ErrorBox("Failed to create window");
+        MessageBox(0, "Failed to create window", "Error", 0);
         return 1;
     }
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
 
-    while (running) {
+    while (state.running) {
         MSG msg;
         while (PeekMessage(&msg, 0, 0, 0, PM_REMOVE)) {
             if (msg.message == WM_KEYDOWN) {
                 if (MessageBox(0, "Really quit?", "", MB_OKCANCEL) == IDOK) {
-                    running = 0;
+                    state.running = 0;
                 }
                 break;
             }
