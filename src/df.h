b81ddc3bc34c83c76a26f6b2fef933bb79f05a90
Dylan Falconer <me@falconerd.com>
2023-12-23 18:30:49 +1100

    .

--- src/df.h
+++ src/df.h
@@ -4,10 +4,13 @@
     - Everything that needs to allocate should accept custom allocators.
     - Allocators will have a standard definition (perhaps look into nullprogram's new article).
     - The most useful things for me personally are prioritised.
+    - Changes will be versioned.
 
     TODO:
         - [ ] Growable array (like vec).
         - [ ] Allocator definition.
 */
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
