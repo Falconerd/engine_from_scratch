872c359291e16e7e14a02ac2bad5c9da6afc7e05
Dylan Falconer <me@falconerd.com>
2023-12-23 18:29:28 +1100

    DF.H begins life

---
+++ src/df.h
@@ -0,0 +1,13 @@
+/*
+    Dylan Falconer's Massive Header Library
+
+    - Everything that needs to allocate should accept custom allocators.
+    - Allocators will have a standard definition (perhaps look into nullprogram's new article).
+    - The most useful things for me personally are prioritised.
+
+    TODO:
+        - [ ] Growable array (like vec).
+        - [ ] Allocator definition.
+*/
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
