7fd74a566a5d065a04d6ca1aae3eed28066df269
Dylan Falconer <me@falconerd.com>
2023-12-29 00:32:15 +1100

    Pie cleanup

--- src/pie.c
+++ src/pie.c
@@ -24,14 +24,11 @@ data: ...
 palette?: ...
 */
 
-void pie_dimensions(byte *bytes, size *width, size *height) {
-    *width = (size)*(u16 *)(&bytes[5]);
-    *height = (size)*(u16 *)(&bytes[7]);
-}
-
 pie_image pie_from_bytes(byte *bytes) {
     pie_image r = {0};
-    pie_dimensions(bytes, &r.width, &r.height);
+
+    r.width = (size)*(u16*)(&bytes[5]);
+    r.height = (size)*(u16*)(&bytes[7]);
     r.version = (u8)bytes[3];
     r.flags = (u8)bytes[4];
     r.length = (size)*(u16 *)(&bytes[9]);
@@ -47,25 +44,17 @@ pie_image pie_from_bytes(byte *bytes) {
 result pie_rgb_from_pie(pie_image p, allocator a) {
     u8rgb *pixels = make(u8rgb, p.width * p.height, a);
     size next_pixel_index = 0;
-    // Assume RGB.
-    u8 step = 3;
+    u8 step = ((p.flags & PIE_FLAG_TRANSPARENCY) > 0) ? 4 : 3;
 
-    // Must be RGBA.
-    if ((p.flags & PIE_FLAG_TRANSPARENCY) > 0) {
-        step = 4;
-    }
+    for (size i = 0; i < p.length; i += 1) {
+        u8 run_length = p.data[i * 2];
+        u8 color_index = p.data[i * 2 + 1] * step;
 
-    if ((p.flags & PIE_FLAG_PALETTE) > 0) {
-        for (size i = 0; i < p.length; i += 1) {
-            u8 run_length = p.data[i * 2];
-            u8 color_index = p.data[i * 2 + 1] * step;
-
-            for (u8 j = 0; j < run_length; j += 1) {
-                size pixel_index = next_pixel_index++;
-                pixels[pixel_index].r = p.palette[color_index + 0];
-                pixels[pixel_index].g = p.palette[color_index + 1];
-                pixels[pixel_index].b = p.palette[color_index + 2];
-            }
+        for (u8 j = 0; j < run_length; j += 1) {
+            size pixel_index = next_pixel_index++;
+            pixels[pixel_index].r = p.palette[color_index + 0];
+            pixels[pixel_index].g = p.palette[color_index + 1];
+            pixels[pixel_index].b = p.palette[color_index + 2];
         }
     }
 
